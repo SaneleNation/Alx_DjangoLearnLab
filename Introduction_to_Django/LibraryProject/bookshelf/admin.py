fb7254740f76f0f6c64872da3cf760530b21c28e
SaneleNation <sanelezwa@gmail.com>
2025-02-18 06:45:45 -0800

    Added modifications and registered model to admin

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Book
 
 # Register your models here.
+admin.site.register(Book)
+
+class BookAdmin(admin.ModelAdmin):
+        list_display = ('title', 'author', 'publication_year')  # Display columns
+            list_filter = ('publication_year',)  # Add filter for publication year
+                search_fields = ('title', 'author')  # Enable search by title and author
